a1df27c28878d4c09d71c21195021d6a503f49b2
hemashree080 <140086440+hemashree080@users.noreply.github.com>
2024-09-22 22:14:12 +0530

    Update users.py

--- core/models/users.py
+++ core/models/users.py
@@ -4,11 +4,13 @@ from core.libs import helpers
 
 class User(db.Model):
     __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
     id = db.Column(db.Integer, db.Sequence('users_id_seq'), primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     created_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False)
     updated_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False, onupdate=helpers.get_utc_now)
+# Assuming this is in your models fil
 
     def __repr__(self):
         return '<User %r>' % self.username
